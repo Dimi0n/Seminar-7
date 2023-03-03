636c4d78847689a9e4673fa936fc80ec29489f65
Dimion <ardgalen@dimlev.ru>
2023-03-03 11:29:12 +0300

    Seminar complete

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿//массив , задать
+//m =3, n = 4
+
+
+// int[,] array = new int [3,4];
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+    
+//     {
+//         array[i,j] = new Random().Next(-10,11);
+//         System.Console.WriteLine(array[i,j], " ");
+//     }
+// }
+
+// // Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// Console.WriteLine("Введите количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[m,n];
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     Console.WriteLine();
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] = i + j;
+//         Console.Write(array[i, j] + " ");
+//     }
+// }
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m,n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    Console.WriteLine();
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 100);
+        if (i == j)
+            //array[i, j] += array[i, j];
+            sum = sum + array[i, j];
+        Console.Write(array[i, j] + " ");
+
+    }
+    Console.WriteLine($"Сумма: {sum}");
+}
